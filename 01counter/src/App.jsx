bfb2f60bf6611e7693c44314f53b0a606760c5aa
Deepak Dhaka <deepakdhaka@cyntexa.com>
2025-08-03 13:08:21 +0530

    viteReact commit

--- 01counter/src/App.jsx
+++ 01counter/src/App.jsx
@@ -4,30 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  let counter = 5
+
+  const addValue = () => {
+    console.log("Counter: " + counter);
+    
+    counter += 1
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Deepak and React</h1>
+      <h2>Counter Value: {counter}</h2>
+
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button>Remove Value</button>
     </>
   )
 }
